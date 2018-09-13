490ad86c033a4ea4405289ba982ed2c683560d7b
knts0 <shimmer22.0204@gmail.com>
2018-09-14 05:11:59 +0900

    日付周りのバグを修正

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Cat
     #config.time_zone = 'Tokyo'
 
     # DB保存時のタイムゾーンをUTCに設定
-    config.active_record.default_timezone = :utc
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
