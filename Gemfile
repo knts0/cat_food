93382729cc3e11ea94ac489165c3e5bcf01cfecb
knts0 <shimmer22.0204@gmail.com>
2018-10-08 18:50:58 +0900

    Userモデルの作成

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ ruby '2.4.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 
+#
+gem 'bcrypt',         '3.1.12'
+
 # Bootstrap
 gem 'bootstrap-sass', '3.3.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_09_120950) do
+ActiveRecord::Schema.define(version: 2018_10_08_094357) do
 
   create_table "logs", force: :cascade do |t|
     t.integer "cat_id"
@@ -20,11 +20,13 @@ ActiveRecord::Schema.define(version: 2018_09_09_120950) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "name", null: false
-    t.boolean "is_done", default: false, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
